bbefaf0b778ea28a58d38875356046ab2a15ec62
Mark Sinclair <mmsinclair@gmail.com>
2022-03-14 11:03:51 +0000

    Form field to enter and display an Identity Key

---
+++ ts-packages/react-components/src/components/mixnodes/IdentityKeyFormField.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { ChangeEvent } from 'react';
+import { InputAdornment, TextField } from '@mui/material';
+import { TextFieldProps } from '@mui/material/TextField/TextField';
+import { validateKey } from '@nymproject/types';
+import DoneIcon from '@mui/icons-material/Done';
+
+export const IdentityKeyFormField: React.FC<{
+  showTickOnValid?: boolean;
+  fullWidth?: boolean;
+  readOnly?: boolean;
+  initialValue?: string;
+  onChanged?: (newValue: string) => void;
+  onValidate?: (isValid: boolean, error?: string) => void;
+  textFieldProps?: TextFieldProps;
+}> = ({ fullWidth, readOnly, initialValue, onChanged, onValidate, textFieldProps, showTickOnValid = true }) => {
+  const [value, setValue] = React.useState<string | undefined>(initialValue);
+  const [validationError, setValidationError] = React.useState<string | undefined>();
+
+  const doValidation = (newValue?: string): boolean => {
+    if (validateKey(newValue)) {
+      setValidationError(undefined);
+      if (onValidate) {
+        onValidate(true);
+      }
+      return true;
+    }
+
+    const newValidationError = 'Key is not valid';
+    setValidationError(newValidationError);
+    if (onValidate) {
+      onValidate(false, newValidationError);
+    }
+
+    return false;
+  };
+
+  React.useEffect(() => {
+    // validate initial value (only if set), so that validation error UI hints are set without the user typing
+    if (initialValue) {
+      doValidation(initialValue);
+    }
+  }, [initialValue]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+
+    if (doValidation(newValue)) {
+      setValue(newValue);
+      if (onChanged) {
+        onChanged(newValue);
+      }
+    }
+  };
+
+  return (
+    <TextField
+      fullWidth={fullWidth}
+      InputProps={{
+        readOnly,
+        startAdornment: showTickOnValid && value && validationError === undefined && (
+          <InputAdornment position="start">
+            <DoneIcon color="success" />
+          </InputAdornment>
+        ),
+      }}
+      {...textFieldProps}
+      aria-readonly={readOnly}
+      error={validationError !== undefined}
+      helperText={validationError}
+      defaultValue={initialValue}
+      onChange={handleChange}
+    />
+  );
+};
---
+++ ts-packages/types/src/utils/keys/index.ts
@@ -0,0 +1,17 @@
+import bs58 from 'bs58';
+
+export const validateKey = (key?: string, bytesLength: number = 32): boolean => {
+  if (!key) {
+    return false;
+  }
+
+  // it must be a valid base58 key
+  try {
+    const bytes = bs58.decode(key);
+    // of length 32
+    return bytes.length === bytesLength;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
