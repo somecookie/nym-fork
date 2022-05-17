fda5bd49f343506ed71bb74221a6fa1dd9191dbc
Mark Sinclair <mmsinclair@gmail.com>
2022-05-17 15:57:04 +0100

    Add separators and extra props to currency components

--- ts-packages/react-components/src/components/currency/Currency.tsx
+++ ts-packages/react-components/src/components/currency/Currency.tsx
@@ -1,17 +1,33 @@
 import * as React from 'react';
 import type { MajorCurrencyAmount } from '@nymproject/types';
+import { Stack, SxProps, Typography } from '@mui/material';
+import { CurrencyWithCoinMark } from './CurrencyWithCoinMark';
+import { CURRENCY_AMOUNT_SPACING, CurrencyAmount } from './CurrencyAmount';
 
 export const Currency: React.FC<{
   majorAmount?: MajorCurrencyAmount;
   showDenom?: boolean;
-}> = ({ majorAmount, showDenom = true }) => {
-  if (!majorAmount) {
-    return <span>-</span>;
+  showCoinMark?: boolean;
+  coinMarkPrefix?: boolean;
+  sx?: SxProps;
+}> = ({ majorAmount, sx, showDenom = true, showCoinMark = false, coinMarkPrefix = false }) => {
+  if (!majorAmount || !majorAmount.amount) {
+    return (
+      <Stack direction="row" sx={sx}>
+        <span>-</span>
+      </Stack>
+    );
+  }
+  if (!showDenom) {
+    return <CurrencyAmount majorAmount={majorAmount} sx={sx} />;
+  }
+  if (showCoinMark) {
+    return <CurrencyWithCoinMark majorAmount={majorAmount} prefix={coinMarkPrefix} sx={sx} />;
   }
   return (
-    <span>
-      {majorAmount.amount}
-      {showDenom && ` ${majorAmount.denom}`}
-    </span>
+    <Stack direction="row" spacing={CURRENCY_AMOUNT_SPACING} sx={sx}>
+      <CurrencyAmount majorAmount={majorAmount} />
+      <span>{majorAmount.denom}</span>
+    </Stack>
   );
 };
